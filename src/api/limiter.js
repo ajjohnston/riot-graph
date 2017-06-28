4b758895ac3125c3222abd6303258a8743e80c2d
Andrew Johnston <ajohnston@riotgames.com>
2017-06-28 13:05:44 -0700

    Added more rate limit values and removed remaining query string.

--- src/api/limiter.js
+++ src/api/limiter.js
@@ -1,4 +1,4 @@
 import Limiter from 'limit-request-promise'
 import config from '../../config.json'
 
-export default new Limiter(config.API.MAX_CALLS_PER_SECOND, 1)
+export default new Limiter(config.API.MAX_CALLS_PER_TIMESPAN, config.API.RATE_TIMESPAN)
--- src/loaders/matchList.js
+++ src/loaders/matchList.js
@@ -1,17 +1,10 @@
 import DataLoader from 'dataloader'
-import qs from 'querystring'
 import getRiotQuery from '../api/api'
 
-const MATCHLIST_ENDPOINT = id => `/lol/match/v3/matchlists/by-account/${id}/recent`
+const MATCHLIST_ENDPOINT = id => `lol/match/v3/matchlists/by-account/${id}/recent`
 
-const getMatchesById = (id, champion) => {
-  const query = qs.stringify({
-    champion,
-  })
+const getMatchesById = id => getRiotQuery(`${MATCHLIST_ENDPOINT(id)}`)
 
-  return getRiotQuery(`${MATCHLIST_ENDPOINT(id)}?${query}`)
-}
-
-export default champions => new DataLoader(
-  ids => Promise.all(ids.map(id => getMatchesById(id, champions)))
+export default new DataLoader(
+  ids => Promise.all(ids.map(getMatchesById))
 )
