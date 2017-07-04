ce9951194b9ea232da3619a7c72e96e090ed5998
Andrew Johnston <andrewjacobjohnston@gmail.com>
2017-07-04 13:21:28 -0700

    Move patch version to config.

--- src/constants/hosts.js
+++ src/constants/hosts.js
@@ -1,5 +1,6 @@
 // @flow
 import { type Region } from './regions'
+import config from '../../config.json'
 
 export const API_HOST = (region: Region) => `https://${region.toLowerCase()}.api.riotgames.com`
-export const IMAGE_HOST = 'http://ddragon.leagueoflegends.com/cdn/7.13.1/img'
+export const IMAGE_HOST = `http://ddragon.leagueoflegends.com/cdn/${config.API.PATCH}/img`
