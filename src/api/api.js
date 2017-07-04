8d369a9432bcfede6598962bac49a7ce2497b854
Andrew Johnston <andrewjacobjohnston@gmail.com>
2017-07-04 13:01:14 -0700

    Updated rate limiter.

--- src/api/api.js
+++ src/api/api.js
@@ -8,6 +8,20 @@ import config from '../../config.json'
 
 const X_RIOT_TOKEN_HEADER = 'X-Riot-Token'
 
+const loggedRequest = (opts: Object) => {
+  logger.debug({
+    uri: opts.uri,
+  })
+
+  return request(opts)
+}
+
+const limitedRequest = limiter(
+  (opts: Object) => loggedRequest(opts),
+  config.API.MAX_CALLS_PER_TIMESPAN,
+  config.API.TIMESPAN_IN_SECONDS
+)
+
 export default function (region: Region, url: string, key: string = config.API.KEY,
   rateLimited: boolean = true) {
   const uri = `${API_HOST(region)}/${url}`
@@ -20,15 +34,11 @@ export default function (region: Region, url: string, key: string = config.API.K
     json: true,
   }
 
-  logger.debug({
-    uri,
-  })
-
   if (!rateLimited) {
-    return request(options)
+    return loggedRequest(options)
   }
 
-  return limiter.req(options)
+  return limitedRequest(options)
     .catch((err: Error) => {
       throw new Error(`url: [${uri}] error: [${err.message}]`)
     })
--- src/api/limiter.js
+++ src/api/limiter.js
@@ -1,4 +1,30 @@
-import Limiter from 'limit-request-promise'
-import config from '../../config.json'
+// @flow
 
-export default new Limiter(config.API.MAX_CALLS_PER_TIMESPAN, config.API.RATE_TIMESPAN)
+export default function (fn: Function, concurrent: number, timespan: number) {
+  const queue = []
+  let count = 0
+  let dequeue
+
+  function timeout() {
+    count -= 1
+    if (queue.length) {
+      dequeue()
+    }
+  }
+
+  dequeue = () => {
+    count += 1
+    const { resolve, args } = queue.shift()
+    resolve(fn(...args))
+    setTimeout(timeout, timespan * 1000)
+  }
+
+  return function limiter(...args: Array<Object>) {
+    return new Promise((resolve: any) => {
+      queue.push({ args, resolve })
+      if (count < concurrent) {
+        dequeue()
+      }
+    })
+  }
+}
