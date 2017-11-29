6053710b1da455e4ba2585132d9c1271557278f1
Andrew Johnston <andrewjacobjohnston@gmail.com>
2017-11-28 22:57:25 -0800

    Fix typo in champion loader

--- src/loaders/champion.js
+++ src/loaders/champion.js
@@ -3,11 +3,11 @@ import DataLoader from 'dataloader'
 import getRiotQuery from '../api/api'
 import { type Region } from '../constants/regions'
 
-const MATCH_ENDPOINT = (id: string) => `lol/static-data/v3/champions/${id}?tags=all`
+const CHAMPION_ENDPOINT = (id: string) => `lol/static-data/v3/champions/${id}?tags=all`
 
-const getMatchById = (region: Region, id: string, key: string) =>
-  getRiotQuery(region, MATCH_ENDPOINT(id), key, false)
+const getChampionById = (region: Region, id: string, key: string) =>
+  getRiotQuery(region, CHAMPION_ENDPOINT(id), key, false)
 
 export default (region: Region, key: string) => new DataLoader(
-  (ids: Array<string>) => Promise.all(ids.map((id: string) => getMatchById(region, id, key)))
+  (ids: Array<string>) => Promise.all(ids.map((id: string) => getChampionById(region, id, key)))
 )
